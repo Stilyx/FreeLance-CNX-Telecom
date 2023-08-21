e2e67669e90c4636adcdc302c33f9ed3b0528838
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-08-21 18:03:59 -0300

    fix (answer texts)

--- src/Messages/questions.ts
+++ src/Messages/questions.ts
@@ -2,53 +2,50 @@ export const questionObj = [
 	{
 		question: "Sou pessoa jurídica, posso me cadastrar nesse modelo de venda?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Sim, tanto pessoas físicas quanto jurídicas podem se cadastrar como parceiros CNX Telecom."
 	},
 	{
 		question: "O que é o Parceiro CNX Telecom?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"O parceiro CNX Telecom é um colaborador independente que divulga e vende nossos produtos e serviços, recebendo comissões por cada venda realizada."
 	},
 	{
 		question: "Sou colaborador CNX Telecom, posso ser parceiro divulgador?",
-		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+		answer: "Sim, colaboradores da CNX Telecom podem também ser parceiros divulgadores."
 	},
 	{
 		question: "Qual é o custo para ser um Parceiro CNX Telecom?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Não há custo para se tornar um Parceiro CNX Telecom. É um processo gratuito e aberto a todos interessados."
 	},
 	{
 		question: "Posso ter mais de uma Vitrine Virtual?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Sim, é possível ter mais de uma Vitrine Virtual, aumentando suas oportunidades de venda."
 	},
 	{
 		question: "Posso cadastrar uma conta salário ou conjunta para receber as comissões?",
-		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+		answer: "Sim, você pode cadastrar uma conta salário ou conjunta para receber as comissões."
 	},
 	{
 		question: "Aposentados podem ser Parceiro CNX Telecom?",
-		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+		answer: "Sim, aposentados são bem-vindos para se tornarem Parceiros CNX Telecom."
 	},
 	{
 		question:
 			"Estou recebendo Seguro-Desemprego, auxílio-doença ou bolsa família pelo INSS, posso criar uma Vitrine Virtual?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Sim, você pode criar uma Vitrine Virtual e tornar-se parceiro mesmo recebendo esses benefícios."
 	},
 	{
 		question: "O que eu ganho por ser Parceiro CNX Telecom?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Como Parceiro CNX Telecom, você ganha comissões por cada venda realizada, além de oportunidades de crescimento e desenvolvimento dentro da empresa."
 	},
 	{
 		question:
 			"Ao vender e receber comissões por essas vendas, eu tenho algum vínculo empregatício com a CNX Telecom?",
 		answer:
-			"ELorem ipsum dolor sit amet, consectetur adipiscing elit Nulla feugiat placerat lorem id lobortis. Ut accumsan ulla"
+			"Não, a parceria com a CNX Telecom não cria um vínculo empregatício. Você atua como um colaborador independente."
 	}
 ];
